a58badeec259fd23897212b5dc517471b18a3724
LiJohnson <598420668@163.com>
2016-04-05 23:21:09 +0800

    添加粘贴文本功能

--- public/js/client.js
+++ public/js/client.js
@@ -1,36 +1,33 @@
-(function (io , $) {
-    console.log($);
+(function (doc , io , $) {
     var socket = new io();
+    var contain = $('.terminaljs');
+    var term = new Terminal( contain.dataset );
+    var stream = ss.createStream({decodeStrings: false, encoding: 'utf-8'});
+
     socket.on('connect',function () {
         console.log('connected');
         socket.emit('join','client');
-    }).on('data',function (params) {
-        console.log(params);
     }).on('message',function (message) {
         alert(message);
     });
-
-    false && $('form').addEventListener('submit',function(e){
-        socket.emit('server',this.elements[0].value);
-        this.elements[0].value = '';
-        e.preventDefault();
-        return false;
-    });
-
-    var contain = $('.terminaljs');
-    var term = window.t=new Terminal( contain.dataset );
-    var stream = window.st=ss.createStream({decodeStrings: false, encoding: 'utf-8'});
 
     stream.write("cd \nsource ~/.profile \n");
     ss(socket).emit('terminal', stream, contain.dataset);
     stream.pipe(term).dom(contain).pipe(stream);
 
+    doc.addEventListener('paste',function(e){
+        console.log(e);
+        var item = e.clipboardData.items[0];
+        var data = item && item.kind == 'string' && e.clipboardData.getData(item.type);
+        data && stream.write(data);
+    });
+
     window.onresize = function(){
-        var size = {rows:contain.clientHeight/17,columns:contain.clientWidth/8};
+        var size = {rows:contain.clientHeight/16,columns:contain.clientWidth/8};
         size.rows = ~~size.rows;
         size.columns = ~~size.columns;
         socket.emit('resize',size);
         term.state.resize(size);
     };
     window.onresize();
-})(window.io , function(s){ return document.querySelector(s); });
+})(document , window.io , function(s){ return document.querySelector(s); });
